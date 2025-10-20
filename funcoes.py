1348287d02c041db70d66735054085b6a547a5b9
tadeu773 <tadeuhbm@al.insper.edu.br>
2025-10-20 10:59:36 -0300

    ep2-1

---
+++ funcoes.py
@@ -0,0 +1,9 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+    elif orientacao == "horizontal":
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+    return posicoes
