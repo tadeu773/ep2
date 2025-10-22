99219a1a27b2c17346d2746cb88907d8f2b5adaa
tadeu773 <tadeuhbm@al.insper.edu.br>
2025-10-22 10:21:44 -0300

    ep2

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,10 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
         for i in range(tamanho):
             posicoes.append([linha, coluna + i])
     return posicoes
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio not in frota:
+        frota[nome_navio] = []
+    frota[nome_navio].append(posicoes)
+    
+    return frota
